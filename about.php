05b423bddc373a8843a35d1584ea1353cf39c65d
@DmitriySkor <rionskey@gmail.com>
2019-08-06 12:23:01 +0300

    add about page, and create first and second section

---
+++ about.php
@@ -0,0 +1,84 @@
+<?php include 'parts/header.php';?>
+
+<div class="about__first_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="about__first_section_text">
+                    Гимнастический комплекс Body Drive изначально создавался как комплекс упражнений,
+                    направленный на восстановление состояния позвоночника и суставов для людей пожилого
+                    возраста людей и людей с серьезными нарушениями деятельности опорно-двигательного
+                    аппарата в результате травматических воздействий. Многие сегодня его знают сегодня
+                    под названием "Гимнастика великих Атлантов".
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class="gymnastiks_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="gymnastiks_skeleton">
+                        <img src="img/skletonn.png" alt="img">
+                        <div class="gymnastiks_section_wrap">
+                            <h2 class="gymnastiks_section_title">
+                                Гимнастика направлена на общее
+                                оздоровление и укрепление
+                                организма
+                            </h2>
+                            <div class="gymnastiks_section_list">
+                                <div class="gymnastiks_section_item">
+                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                    <p class="gymnastiks_item_text">
+                                        Перестают беспокоить грыжи, протрузии и нестабильные позвонки
+                                    </p>
+                                </div>
+                                <div class="gymnastiks_section_item">
+                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                    <p class="gymnastiks_item_text">
+                                        Исчезают ноющие боли в спине
+                                        и мышцах
+                                    </p>
+                                </div>
+                                <div class="gymnastiks_section_item">
+                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                    <p class="gymnastiks_item_text">
+                                        Проходят последствия
+                                        сорванной спины
+                                    </p>
+                                </div>
+                                <div class="gymnastiks_section_item">
+                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                    <p class="gymnastiks_item_text">
+                                        Восстанавливается естественная
+                                        осанка и проходит желание сутулиться
+                                    </p>
+                                </div>
+                                <div class="gymnastiks_section_item">
+                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                    <p class="gymnastiks_item_text">
+                                        Значительно улучшается подвижность
+                                        суставов, уходит болевой синдром
+                                    </p>
+                                </div>
+                                <div class="gymnastiks_section_item">
+                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                    <p class="gymnastiks_item_text">
+                                        Нормализуется давление
+                                        и сердечный ритм
+                                    </p>
+                                </div>
+                            </div>
+                            <button class="gymnastiks_btn red_button">Записаться на занятие</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php include 'parts/green_form.php';?>
+<?php include 'parts/footer.php';?>
