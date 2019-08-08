268b1c3dd141d3b4046940a5e120a8051bbf21da
@DmitriySkor <rionskey@gmail.com>
2019-08-08 17:56:09 +0300

    about page, add five section and six section.

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<?php include 'parts/header.php';?>
+<?php include 'parts/header.php'; ?>
 
 <div class="about__first_section">
     <div class="container">
@@ -16,88 +16,88 @@
     </div>
 </div>
 
-    <div class="gymnastiks_section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="gymnastiks_skeleton">
-                        <div class="gym_lesson">
-                            <p class="gym_lesson_title">
+<div class="gymnastiks_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="gymnastiks_skeleton">
+                    <div class="gym_lesson">
+                        <p class="gym_lesson_title">
                             1-2 <br> <span>занятия</span>
-                            </p>
-                            <p class="gym_lesson_sub_title">
-                                Практика показала, что эффект заметен<br>
-                                уже после одного-двух занятий.
-                            </p>
-                        </div>
-                        <img src="img/skletonn.png" alt="img">
-                        <div class="gymnastiks_section_wrap">
-                            <h2 class="gymnastiks_section_title about__gym_section_title">
-                                Здоровый позвоночник - стройное тело!
-                            </h2>
-                            <div class="gymnastiks_section_list">
-                                <div class="gymnastiks_section_item">
-                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
-                                    <p class="gymnastiks_item_text">
-                                        Перестают беспокоить грыжи, протрузии и нестабильные позвонки
-                                    </p>
-                                </div>
-                                <div class="gymnastiks_section_item">
-                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
-                                    <p class="gymnastiks_item_text">
-                                        Исчезают ноющие боли в спине
-                                        и мышцах
-                                    </p>
-                                </div>
-                                <div class="gymnastiks_section_item">
-                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
-                                    <p class="gymnastiks_item_text">
-                                        Проходят последствия
-                                        сорванной спины
-                                    </p>
-                                </div>
-                                <div class="gymnastiks_section_item">
-                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
-                                    <p class="gymnastiks_item_text">
-                                        Восстанавливается естественная
-                                        осанка и проходит желание сутулиться
-                                    </p>
-                                </div>
-                                <div class="gymnastiks_section_item">
-                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
-                                    <p class="gymnastiks_item_text">
-                                        Значительно улучшается подвижность
-                                        суставов, уходит болевой синдром
-                                    </p>
-                                </div>
-                                <div class="gymnastiks_section_item">
-                                    <img src="img/item_dott.png" alt="img" width="16" height="16">
-                                    <p class="gymnastiks_item_text">
-                                        Нормализуется давление
-                                        и сердечный ритм
-                                    </p>
-                                </div>
+                        </p>
+                        <p class="gym_lesson_sub_title">
+                            Практика показала, что эффект заметен<br>
+                            уже после одного-двух занятий.
+                        </p>
+                    </div>
+                    <img src="img/skletonn.png" alt="img">
+                    <div class="gymnastiks_section_wrap">
+                        <h2 class="gymnastiks_section_title about__gym_section_title">
+                            Здоровый позвоночник - стройное тело!
+                        </h2>
+                        <div class="gymnastiks_section_list">
+                            <div class="gymnastiks_section_item">
+                                <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                <p class="gymnastiks_item_text">
+                                    Перестают беспокоить грыжи, протрузии и нестабильные позвонки
+                                </p>
                             </div>
-                            <button class="gymnastiks_btn red_button">Записаться на занятие</button>
+                            <div class="gymnastiks_section_item">
+                                <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                <p class="gymnastiks_item_text">
+                                    Исчезают ноющие боли в спине
+                                    и мышцах
+                                </p>
+                            </div>
+                            <div class="gymnastiks_section_item">
+                                <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                <p class="gymnastiks_item_text">
+                                    Проходят последствия
+                                    сорванной спины
+                                </p>
+                            </div>
+                            <div class="gymnastiks_section_item">
+                                <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                <p class="gymnastiks_item_text">
+                                    Восстанавливается естественная
+                                    осанка и проходит желание сутулиться
+                                </p>
+                            </div>
+                            <div class="gymnastiks_section_item">
+                                <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                <p class="gymnastiks_item_text">
+                                    Значительно улучшается подвижность
+                                    суставов, уходит болевой синдром
+                                </p>
+                            </div>
+                            <div class="gymnastiks_section_item">
+                                <img src="img/item_dott.png" alt="img" width="16" height="16">
+                                <p class="gymnastiks_item_text">
+                                    Нормализуется давление
+                                    и сердечный ритм
+                                </p>
+                            </div>
                         </div>
+                        <button class="gymnastiks_btn red_button">Записаться на занятие</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 <div class="about__complex">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="complex_img_wrap">
-            <img class="complex_img" src="img/about-complex.png" alt="img">
+                    <img class="complex_img" src="img/about-complex.png" alt="img">
                 </div>
-            <div class="about__complex_text">
-                <p>Комплекс Body Drive объединил в себе и традиционные элементы лечебной физкультуры,
-                и фундаментальные основы китайской медицины, йоги и других восточных практик, что
-                делает ее уникальной по своим свойствам и оказываемому эффекту.</p>
-            </div>
+                <div class="about__complex_text">
+                    <p>Комплекс Body Drive объединил в себе и традиционные элементы лечебной физкультуры,
+                        и фундаментальные основы китайской медицины, йоги и других восточных практик, что
+                        делает ее уникальной по своим свойствам и оказываемому эффекту.</p>
+                </div>
             </div>
         </div>
     </div>
@@ -106,6 +106,7 @@
 <div class="about__advantages">
     <div class="container">
         <div class="row">
+
             <div class="about__advantage_one">
                 <div class="col-md-3">
                     <p class="about_advantage_title">
@@ -114,26 +115,156 @@
                 </div>
                 <div class="col-md-3">
                     <div class="about_advantage_item">
-                    <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
-                    <p class="about_advantage_text">
-                        Восстановление двигательной активности
-                    </p>
+                        <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                        <p class="about_advantage_text">
+                            Восстановление двигательной активности
+                        </p>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="about_advantage_item">
-                    <img class="about__advantage_img" src="img/ico-advantage2.png" alt="img">
-                    <p class="about_advantage_text">
-                        Общее омоложение организма
-                    </p>
+                        <img class="about__advantage_img" src="img/ico-advantage2.png" alt="img">
+                        <p class="about_advantage_text">
+                            Общее омоложение организма
+                        </p>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="about_advantage_item">
-                    <img class="about__advantage_img" src="img/ico-advantage3.png" alt="img">
-                    <p class="about_advantage_text">
-                        Повышение потенции даже у пожилых людей
+                        <img class="about__advantage_img" src="img/ico-advantage3.png" alt="img">
+                        <p class="about_advantage_text">
+                            Повышение потенции даже у пожилых людей
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="about__advantage_two">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+
+                            <div class="about_advantage_wrap">
+
+                                <div class="advantage_two_title">
+                                    <p>Для тех, у кого</p>
+                                </div>
+
+                                <div class="about_advanced_list">
+
+                                    <ul>
+                                        <li>
+                                            <div class="col-md-4">
+                                            <div class="about_advantage_item">
+                                                <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                                                <p class="about_advantage_text">
+                                                    Избыточный вес
+                                                </p>
+                                            </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                            <div class="about_advantage_item">
+                                                <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                                                <p class="about_advantage_text">
+                                                    Нарушение осанки
+                                                </p>
+                                            </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                            <div class="about_advantage_item">
+                                                <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                                                <p class="about_advantage_text">
+                                                    Боли в спине
+                                                </p>
+                                            </div>
+                                            </div>
+                                        </li>
+
+                                        <li>
+                                            <div class="col-md-4">
+                                            <div class="about_advantage_item">
+                                                <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                                                <p class="about_advantage_text">
+                                                    Головные боли
+                                                </p>
+                                            </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                            <div class="about_advantage_item">
+                                                <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                                                <p class="about_advantage_text">
+                                                    Мышечная слабость
+                                                </p>
+                                            </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                            <div class="about_advantage_item">
+                                                <img class="about__advantage_img" src="img/ico-advantage1.png" alt="img">
+                                                <p class="about_advantage_text">
+                                                    Депрессия и хроническая усталость
+                                                </p>
+                                            </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+</div>
+
+<div class="body_course_description about_course_description">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 body_course_left">
+                <div class="body_course_pic">
+                    <img src="img/about-cource-img.png" alt="img">
+                </div>
+            </div>
+            <div class="col-md-6 body_course_right">
+                <div class="course_description_text">
+                    <p>На сегодняшний день – это полноценная фитнес-программа,
+                        которая за короткий срок не только оказывает комплексный
+                        терапевтический эффект на весь опорно-двигательный аппарат
+                        в целом, но и приводит к снижению веса, восстановлению тонуса
+                        организма и, как следствие, качественному повышению уровня жизни.
                     </p>
+                </div>
+                <div class="course_medical_wrap">
+                    <div class="course_medical_justification">
+                        <a href="#" class="justification_title">
+                            <img class="justification_emb" src="img/justificsation-title.png" alt="img">
+                            <p>Медицинское обоснование
+                                комплекса Body Drive</p>
+                            <img class="justification-arrow" src="img/justification-arrow.png" alt="img">
+                        </a>
+                    </div>
+                    <div class="autor_justification">
+                        <div class="autor_justification_img">
+                            <img src="img/autor_justification.png" alt="img" width="50" height="50">
+                        </div>
+                        <div class="autor_justification_desk">
+                            <p class="autor_justification_name">
+                                Бунин Арсений Владимирович
+                            </p>
+                            <p class="autor_justification_position">
+                                Зав. отделением, врач по лечебной физкультуре ФКУ ЦВКГ
+                                им.П.В.Мандрыка МО РФ (Медицинский центр восстановительного
+                                лечения, Королёв).
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -142,5 +273,5 @@
 </div>
 
 
-<?php include 'parts/green_form.php';?>
-<?php include 'parts/footer.php';?>
+<?php include 'parts/green_form.php'; ?>
+<?php include 'parts/footer.php'; ?>
